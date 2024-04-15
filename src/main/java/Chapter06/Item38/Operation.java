52cae6ce2b69abefb1d0957c6011e221ede8dc27
plasys <kezy@daum.net>
2024-04-15 21:54:49 +0900

    240415

---
+++ src/main/java/Chapter06/Item38/Operation.java
@@ -0,0 +1,5 @@
+package Chapter06.Item38;
+
+public interface Operation {
+    double apply(double x, double y);
+}
---
+++ src/main/java/Chapter06/Item38/Test.java
@@ -0,0 +1,11 @@
+package Chapter06.Item38;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("BasicOperation.PLUS.apply(2, 1) = " + BasicOperation.PLUS.apply(2, 1));
+        System.out.println("BasicOperation.MINUS.apply(2,1) = " + BasicOperation.MINUS.apply(2,1));
+        System.out.println("BasicOperation.MINUS.apply(2,1) = " + BasicOperation.TIMES.apply(2,1));
+        System.out.println("BasicOperation.MINUS.apply(2,1) = " + BasicOperation.DIVIDE.apply(2,1));
+    }
+}
