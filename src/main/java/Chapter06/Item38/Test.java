0a87cc9d9dcea084b43e5418ca9917a5b5882af9
plasys <kezy@daum.net>
2024-05-06 22:13:37 +0900

    240506

--- src/main/java/Chapter06/Item38/Test.java
+++ src/main/java/Chapter06/Item38/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("BasicOperation.PLUS.apply(2, 1) = " + BasicOperation.PLUS.apply(2, 1));
         System.out.println("BasicOperation.MINUS.apply(2,1) = " + BasicOperation.MINUS.apply(2,1));
-        System.out.println("BasicOperation.MINUS.apply(2,1) = " + BasicOperation.TIMES.apply(2,1));
-        System.out.println("BasicOperation.MINUS.apply(2,1) = " + BasicOperation.DIVIDE.apply(2,1));
+        System.out.println("BasicOperation.TIMES.apply(2,1) = " + BasicOperation.TIMES.apply(2,1));
+        System.out.println("BasicOperation.DIVIDE.apply(2,1) = " + BasicOperation.DIVIDE.apply(2,1));
     }
 }
